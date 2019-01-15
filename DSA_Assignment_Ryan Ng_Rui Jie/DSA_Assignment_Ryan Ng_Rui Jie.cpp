c9243407d0edab1a949c2a8f87ee2c5fd451b9a1
Rui Jie Tay <s10178023@connect.np.edu.sg>
2019-01-15 19:33:13 +0800

    Created menu

--- DSA_Assignment_Ryan Ng_Rui Jie/DSA_Assignment_Ryan Ng_Rui Jie.cpp
+++ DSA_Assignment_Ryan Ng_Rui Jie/DSA_Assignment_Ryan Ng_Rui Jie.cpp
@@ -1,8 +1,50 @@
 #include "pch.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int option;
 
 int main()
 {
-	//hello
+	while (true) {
+
+		Menu();
+
+		switch (option) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 0:
+			cout << "Bye!" << endl;
+			system("PAUSE");
+			return 0;
+		}
+	}
 }
 
+void Menu() {
+	cout << endl << "---------------- Spell Checker -------------------" << endl;
+	cout << "[1] Spell Check a Word" << endl;
+	cout << "[2] Spell Check a File" << endl;
+	cout << "[3] Add a New Word to the Dictionary" << endl;
+	cout << "[4] Save the Dictionary (With New Words Added)" << endl;
+	cout << "[5] Display All Words in the Dictionary that Starts with a Certain Letter" << endl;
+	cout << "[0] Exit " << endl;
+	cout << "----------------------------------------------" << endl;
+	cout << "Enter your option : ";
+
+	cin >> option;
+
+	cout << endl;
+}
